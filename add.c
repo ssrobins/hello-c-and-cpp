14491642a599761c0ac0420e5bf8bd32bfb201c2
Steve Robinson <ssrobins@gmail.com>
2021-09-09 22:18:04 -0700

    Add an example of C and C++ source files in a static lib

---
+++ add.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int addc(int number1, int number2)
+{
+    printf("Add %d and %d", number1, number2);
+    return number1 + number2;
+}
---
+++ add.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int addcpp(int number1, int number2)
+{
+    std::cout << "Add " << number1 << " and " << number2 << std::endl;
+    return number1 + number2;
+}
